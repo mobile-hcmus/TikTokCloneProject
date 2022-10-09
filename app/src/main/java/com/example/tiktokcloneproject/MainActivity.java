72ec7bb3c5a2a5a5ee13011b421a520e1060c46f
Duy Quang <vanduyquang2020@gmail.com>
2022-10-09 20:25:54 +0700

    TIKTOK-36

--- app/src/main/java/com/example/tiktokcloneproject/MainActivity.java
+++ app/src/main/java/com/example/tiktokcloneproject/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// a
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
