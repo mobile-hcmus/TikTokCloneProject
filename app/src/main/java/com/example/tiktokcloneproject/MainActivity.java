76fff638c9c97c51d3699e4ac3f3fba538c50f71
duytruong-23 <zduytruongz@gmail.com>
2022-10-10 09:38:03 +0700

    TIKTOK-41 testing

--- app/src/main/java/com/example/tiktokcloneproject/MainActivity.java
+++ app/src/main/java/com/example/tiktokcloneproject/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //commit test
         //test duytruong
+        //test
     }
     //mtoan
 }
