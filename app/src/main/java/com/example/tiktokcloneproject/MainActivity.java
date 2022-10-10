3f47d8cb1d5309882a48e7b0f04300be51210e5f
duytruong-23 <zduytruongz@gmail.com>
2022-10-10 09:37:12 +0700

    git commit -m "TIKTOK-41 testing"

--- app/src/main/java/com/example/tiktokcloneproject/MainActivity.java
+++ app/src/main/java/com/example/tiktokcloneproject/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //commit test
+        //test duytruong
     }
     //mtoan
 }
