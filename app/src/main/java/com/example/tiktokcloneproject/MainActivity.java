e744a61428d44fe075d8a0349a6c91aacdb7d53a
Duy Quang <vanduyquang2020@gmail.com>
2022-10-11 15:49:31 +0700

    TIKTOK-36 Add Firebase

--- app/src/main/java/com/example/tiktokcloneproject/MainActivity.java
+++ app/src/main/java/com/example/tiktokcloneproject/MainActivity.java
@@ -2,19 +2,13 @@ package com.example.tiktokcloneproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-// a
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //commit test
-        //test duytruong
-        //test
     }
-    //mtoan
-    //quynh_testmessage
-
 }
