37cafa4626240ef0e908f36db5dcbc5e07222737
20120125 <20120215@student.hcmus.edu.vn>
2022-10-12 11:30:05 +0700

    TIKTOK-29 giao dien DeleteAccount

---
+++ app/src/main/java/com/example/tiktokcloneproject/DeleteAccountActivity.java
@@ -0,0 +1,14 @@
+package com.example.tiktokcloneproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DeleteAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_account);
+    }
+}
