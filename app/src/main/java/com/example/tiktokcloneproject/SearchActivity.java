cd1d26764f933216f247ac085e28fe795e4e13e9
20120188 <20120188@student.hcmus.edu.vn>
2022-10-15 11:31:31 +0700

    TIKTOK-44

--- app/src/main/java/com/example/tiktokcloneproject/SearchActivity.java
+++ app/src/main/java/com/example/tiktokcloneproject/SearchActivity.java
@@ -9,8 +9,9 @@ public class SearchActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
         setContentView(R.layout.activity_searching);
+
+
+
     }
 }
