6f7c145bee1561af93daddd68c43b0adafaf8b62
20120125 <20120215@student.hcmus.edu.vn>
2022-10-15 09:03:57 +0700

    TIKTOK-29 thiet lap nut Ho So chuyen sang man hinh dang nhap, tam thoi chua xu ly da dang nhap hay chua

--- app/src/main/java/com/example/tiktokcloneproject/HomeScreenActivity.java
+++ app/src/main/java/com/example/tiktokcloneproject/HomeScreenActivity.java
@@ -2,13 +2,33 @@ package com.example.tiktokcloneproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HomeScreenActivity extends AppCompatActivity {
+public class HomeScreenActivity extends Activity implements View.OnClickListener{
+
+    private Button btnProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        btnProfile = (Button) findViewById(R.id.btnProfile);
+
+        btnProfile.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == btnProfile.getId())
+        {
+            Intent intent = new Intent(HomeScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+
+            finish();
+        }
     }
 }
