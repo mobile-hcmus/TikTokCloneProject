054eaeab38e4d60d5b04a1d4e83d5dcc0f0fc058
20120188 <20120188@student.hcmus.edu.vn>
2022-10-15 15:51:54 +0700

    TIKTOK-44

--- app/src/main/java/com/example/tiktokcloneproject/SearchActivity.java
+++ app/src/main/java/com/example/tiktokcloneproject/SearchActivity.java
@@ -5,11 +5,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class SearchActivity extends Activity {
+
+
+
+
+
+    private FirebaseFirestore db;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searching);
+
+        db = FirebaseFirestore.getInstance();
+
+
+
+
+
+
 
 
 
