85024fadb96eac81deaaa9362a76725ab39dc456
duytruong-23 <zduytruongz@gmail.com>
2022-10-17 09:31:03 +0700

    TIKTOK-2 update

--- app/src/main/java/com/example/tiktokcloneproject/ProfileActivity.java
+++ app/src/main/java/com/example/tiktokcloneproject/ProfileActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ProfileActivity extends AppCompatActivity {
 
     @Override
@@ -16,6 +18,14 @@ public class ProfileActivity extends AppCompatActivity {
 
     public void privacyPage(View view) {
         Intent intent = new Intent(ProfileActivity.this, DeleteAccountActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void signOut(View v)
+    {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(ProfileActivity.this, HomeScreenActivity.class);
         startActivity(intent);
         finish();
     }
--- app/src/main/java/com/example/tiktokcloneproject/SigninPage.java
+++ app/src/main/java/com/example/tiktokcloneproject/SigninPage.java
@@ -1,11 +1,13 @@
 package com.example.tiktokcloneproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -13,13 +15,27 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.concurrent.TimeUnit;
+
 public class SigninPage extends Activity implements View.OnClickListener {
-    private LinearLayout llSigninPage, llChoice, llPhone, llWait;
-    private EditText edtPhone, edtPassword, edtConfirm;
-    private Button btnPhone, btnChoicePhone, btnChoiceEmail, btnChoiceFacebook, btnBackToHomeScreen,
+    private LinearLayout llSigninPage, llChoice, llPhone, llWait, llOtp;
+    private EditText edtPhone, edtPassword, edtOtp;
+    private Button btnPhone, btnOtp, btnChoicePhone, btnChoiceEmail, btnChoiceFacebook, btnBackToHomeScreen,
             btnBackToChoice;
     private final int VISIBLE = View.VISIBLE;
     private final int GONE = View.GONE;
@@ -27,7 +43,14 @@ public class SigninPage extends Activity implements View.OnClickListener {
     private String msg;
     Handler handler = new Handler();
     /////firebase///////
+    String mVerificationId;
+    PhoneAuthProvider.ForceResendingToken mResendToken;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks callbacks;
+    FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
     private FirebaseFirestore db;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,11 +59,13 @@ public class SigninPage extends Activity implements View.OnClickListener {
         llSigninPage = (LinearLayout) findViewById(R.id.llSigninPage);
         llPhone = (LinearLayout) llSigninPage.findViewById(R.id.llPhone);
         llChoice = (LinearLayout) llSigninPage.findViewById(R.id.llChoice);
+        llOtp = (LinearLayout) llSigninPage.findViewById(R.id.llOtp);
         llWait = (LinearLayout) llSigninPage.findViewById(R.id.llWait);
         edtPhone = (EditText) llSigninPage.findViewById(R.id.edtPhone);
+        edtOtp = (EditText) llSigninPage.findViewById(R.id.edtOtp);
         edtPassword = (EditText) llSigninPage.findViewById(R.id.edtPassword);
-        edtConfirm = (EditText) llSigninPage.findViewById(R.id.edtConfirm);
         btnPhone = (Button) llSigninPage.findViewById(R.id.btnPhone);
+        btnOtp = (Button) llSigninPage.findViewById(R.id.btnOtp);
         btnChoicePhone = (Button) llSigninPage.findViewById(R.id.btnChoicePhone);
         btnChoiceEmail = (Button) llSigninPage.findViewById(R.id.btnChoiceEmail);
         btnChoiceFacebook = (Button) llSigninPage.findViewById(R.id.btnChoiceFacebook);
@@ -49,11 +74,46 @@ public class SigninPage extends Activity implements View.OnClickListener {
 
         setVisibleVisibility(llChoice.getId());
         btnPhone.setText(getString(R.string.ic_btnLogin) + getString(R.string.ic_btnLogin));
+        btnOtp.setText(getString(R.string.ic_btnLogin) + getString(R.string.ic_btnLogin));
 
         db = FirebaseFirestore.getInstance();
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+        //////callbacks//////
+        callbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+                String code = phoneAuthCredential.getSmsCode();
+                if(code != null) {
+                    edtOtp.setText(code);
+                    signInWithPhoneAuthCredential(phoneAuthCredential);
+                }
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                Toast.makeText(SigninPage.this, getString(R.string.error_verify), Toast.LENGTH_SHORT).show();
+                setVisibleVisibility(llPhone.getId());
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId,
+                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                super.onCodeSent(verificationId, token);
+                Toast.makeText(SigninPage.this, getString(R.string.otp_sent), Toast.LENGTH_SHORT).show();
+                setVisibleVisibility(llOtp.getId());
+                mVerificationId = verificationId;
+                mResendToken = token;
+
+            }
+        }; // end callbacks
+
+
         btnChoicePhone.setOnClickListener(this);
         btnPhone.setOnClickListener(this);
+        btnOtp.setOnClickListener(this);
         btnBackToChoice.setOnClickListener(this);
         btnBackToHomeScreen.setOnClickListener(this);
     }//on create
@@ -71,7 +131,27 @@ public class SigninPage extends Activity implements View.OnClickListener {
         if(v.getId() == btnPhone.getId()) {
            handleBtnPhoneClick();
         }
+        if(v.getId() == btnOtp.getId()) {
+            if(TextUtils.isEmpty(edtOtp.getText().toString()) || edtOtp.getText().toString().length() != 6) {
+                Toast.makeText(SigninPage.this, getString(R.string.error_Otp), Toast.LENGTH_SHORT).show();
+            }
+            else {
+                PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, edtOtp.getText().toString());
+                signInWithPhoneAuthCredential(credential);
+            }
+        }
 
+    }
+
+    private void signIn() {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(edtPhone.getText().toString())       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(SigninPage.this)                 // Activity (for callback binding)
+                        .setCallbacks(callbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
     }
 
     private void setVisibleVisibility(int id) {
@@ -89,6 +169,26 @@ public class SigninPage extends Activity implements View.OnClickListener {
 
         finish();
     }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        FirebaseAuth.getInstance().signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Toast.makeText(SigninPage.this, getString(R.string.successful_signin), Toast.LENGTH_SHORT).show();
+                            moveToAnotherActivity(HomeScreenActivity.class);
+
+                        } else {
+
+                            setVisibleVisibility(llPhone.getId());
+
+                        }
+
+                    }
+                });
+    }
+
 
     private void handleBtnPhoneClick() {
         String phone = edtPhone.getText().toString();
@@ -121,8 +221,7 @@ public class SigninPage extends Activity implements View.OnClickListener {
                             Toast.makeText(SigninPage.this, getString(R.string.error_signin), Toast.LENGTH_SHORT).show();
                             setVisibleVisibility(llPhone.getId());
                         } else {
-                            Toast.makeText(SigninPage.this, getString(R.string.successful_signin), Toast.LENGTH_SHORT).show();
-                            moveToAnotherActivity(HomeScreenActivity.class);
+                            handler.post(this::signIn);
                         }
                     });
             setVisibleVisibility(llWait.getId());
