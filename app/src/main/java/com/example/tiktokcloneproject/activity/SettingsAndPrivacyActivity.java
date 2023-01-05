d69a8884764eabd4c089e058a747d57c205cff14
LeThaiNhuQuynh <lethainhuquynh@gmail.com>
2023-01-05 13:48:01 +0700

    TIKTOK-67 Share Profile in Setting

--- app/src/main/java/com/example/tiktokcloneproject/activity/SettingsAndPrivacyActivity.java
+++ app/src/main/java/com/example/tiktokcloneproject/activity/SettingsAndPrivacyActivity.java
@@ -3,19 +3,26 @@ package com.example.tiktokcloneproject.activity;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.tiktokcloneproject.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SettingsAndPrivacyActivity extends AppCompatActivity {
 
     private ImageView imvBackToProfile;
     private FrameLayout flAccountOption, flPrivacyOption, flShareProfileOption;
-
+    FirebaseAuth auth;
+    FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +36,9 @@ public class SettingsAndPrivacyActivity extends AppCompatActivity {
         flAccountOption = (FrameLayout) findViewById(R.id.flAccountOption);
         flPrivacyOption = (FrameLayout) findViewById(R.id.flPrivacyOption);
         flShareProfileOption = (FrameLayout) findViewById(R.id.flShareProfileOption);
+
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
 
         imvBackToProfile.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,7 +62,11 @@ public class SettingsAndPrivacyActivity extends AppCompatActivity {
 
         flShareProfileOption.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view) {ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("toptop-link", "http://toptoptoptop.com/" + user.getUid().toString());
+                clipboard.setPrimaryClip(clip);
+                Toast.makeText(SettingsAndPrivacyActivity.this, "Profile link has been saved to clipboard", Toast.LENGTH_SHORT).show();
+
 
             }
         });
